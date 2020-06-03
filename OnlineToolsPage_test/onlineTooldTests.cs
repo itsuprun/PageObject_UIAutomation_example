f985929d38a656259ced70b2d36038830b98ae24
Ilya Tsuprun <itsuprik@gmail.com>
2020-06-03 19:59:06 +0300

    Initial commit

---
+++ OnlineToolsPage_test/onlineTooldTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace OnlineToolsPage
+{
+    [TestFixture]
+    public class OnlineToolsTests
+    {
+        private IWebDriver driver;
+        private string _url = "http://emn178.github.io/online-tools/";
+        
+        [SetUp]
+        public void TestInitialize()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl(_url);
+        }
+
+        [TearDown]
+        public void TestFinalize()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void sha256Test()
+        {
+            var onlineToolsPage = new OnlineToolsPage(driver);
+            onlineToolsPage.ClickButton(onlineToolsPage.sha256Button);
+            onlineToolsPage.fillInputArea(onlineToolsPage.inputArea,"nvdnsjjasn");
+            onlineToolsPage.ClickButton(onlineToolsPage.checkButton);
+            onlineToolsPage.ClickButton(onlineToolsPage.hashButton);
+            Assert.IsTrue(onlineToolsPage.sha256TestPassed);
+        }
+        
+        
+    }
+}
